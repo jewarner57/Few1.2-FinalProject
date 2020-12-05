d60da0e0aa96b1cbba119a357c57827ea29012e0
Jonathan Warner <jewarner57@gmail.com>
2020-12-05 16:58:21 -0500

    added flashing iam list on home page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,22 @@
 import './Hero.css'
+import React, { useState } from 'react'
 
 function Hero() {
+    const [descriptors, setDescriptors] = useState(["Front-End Developer", "App Developer", "Problem Solver", "Web Developer", "Game Developer"]);
+    const [counter, setCounter] = useState(0)
+
+    React.useEffect(() => {
+        const timer =
+            setInterval(() => setCounter(counter + 1), 3000);
+        return () => clearInterval(timer);
+    }, [counter]);
+
     return (
         <div className="Hero" id="pagetop">
             <div className="headerImage" id="pageTop">
                 <div className="headerTitle">
                     <h1>Jonathan Warner:</h1>
-                    <h2 id="iamList">Front-End Developer</h2>
+                    <h2 id="iamList">{descriptors[counter % descriptors.length]}</h2>
                 </div>
             </div>
         </div>
