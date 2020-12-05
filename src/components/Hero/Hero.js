81c0a634a25758b4171b1af32d6f158b165e2562
Jonathan Warner <jewarner57@gmail.com>
2020-12-05 16:28:41 -0500

    updated title positioning and sizing

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,12 +3,12 @@ import './Hero.css'
 function Hero() {
     return (
         <div className="Hero" id="pagetop">
-            <div className="headerTitle">
-                <h1>Jonathan Warner:</h1>
-                <h2 id="iamList">Front-End Developer</h2>
+            <div className="headerImage" id="pageTop">
+                <div className="headerTitle">
+                    <h1>Jonathan Warner:</h1>
+                    <h2 id="iamList">Front-End Developer</h2>
+                </div>
             </div>
-
-            <div className="headerImage" id="pageTop"></div>
         </div>
     );
 }
