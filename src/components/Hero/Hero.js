b0f8a0493ca88f57d0de4eb258fa836fd1bd0970
Jonathan Warner <jewarner57@gmail.com>
2021-06-30 17:29:29 -0400

    fix: update hero title

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,25 +2,25 @@ import './Hero.css'
 import React, { useState } from 'react'
 
 function Hero() {
-    const [descriptors] = useState(["Front-End Developer", "App Developer", "Problem Solver", "Web Developer", "Game Developer"]);
-    const [counter, setCounter] = useState(0)
+  const [descriptors] = useState(["Software Developer", "Front-End Developer", "App Developer", "Problem Solver", "Web Developer", "Game Developer"]);
+  const [counter, setCounter] = useState(0)
 
-    React.useEffect(() => {
-        const timer =
-            setInterval(() => setCounter(counter + 1), 3000);
-        return () => clearInterval(timer);
-    }, [counter]);
+  React.useEffect(() => {
+    const timer =
+      setInterval(() => setCounter(counter + 1), 3000);
+    return () => clearInterval(timer);
+  }, [counter]);
 
-    return (
-        <div className="Hero" id="pagetop">
-            <div className="headerImage" id="pageTop">
-                <div className="headerTitle">
-                    <h1>Jonathan Warner:</h1>
-                    <h2 id="iamList">{descriptors[/*counter % descriptors.length*/0]}</h2>
-                </div>
-            </div>
+  return (
+    <div className="Hero" id="pagetop">
+      <div className="headerImage" id="pageTop">
+        <div className="headerTitle">
+          <h1>Jonathan Warner:</h1>
+          <h2 id="iamList">{descriptors[/*counter % descriptors.length*/0]}</h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Hero
